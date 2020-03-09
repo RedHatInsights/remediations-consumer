496c909117b198b0154fed39a08e4692b7da19d3
Jozef Hartinger <jharting@redhat.com>
2020-03-09 10:55:07 +0100

    use the correct receptor topic
    https://projects.engineering.redhat.com/browse/RHCLOUD-4369

--- src/handlers/receptor.integration.ts
+++ src/handlers/receptor.integration.ts
@@ -5,7 +5,7 @@ import handler from './receptor';
 describe('receptor handler integration tests', function () {
     test('prints message to receptor probe', async () => {
         const message = {
-            topic: 'platform.receptor-controller.jobs',
+            topic: 'platform.receptor-controller.responses',
             value: '{"account": "00001", "sender": "fifi", "message_id": "6cfa75ee-5ba9-442e-9557-6dbbf33593c4", "message_type": "update", "payload": "{}"}',
             offset: 0,
             partition: 58,
