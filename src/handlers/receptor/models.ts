e66a9c1b0b5bd7bc7c0ecfa07fbe5d3bb5f73ddb
Jozef Hartinger <jharting@redhat.com>
2020-03-23 13:02:36 +0100

    RHCLOUD-5298 Implement logic for playbook_run_update

---
+++ src/handlers/receptor/models.ts
@@ -0,0 +1,35 @@
+export enum Status {
+    PENDING = 'pending',
+    ACKED = 'acked',
+    RUNNING = 'running',
+    SUCCESS = 'success',
+    FAILURE = 'failure',
+    CANCELED = 'canceled'
+}
+
+export enum PlaybookRun {
+    TABLE = 'playbook_runs',
+    id = 'id',
+    status = 'status',
+    updated_at = 'updated_at'
+}
+
+export enum PlaybookRunExecutor {
+    TABLE = 'playbook_run_executors',
+    id = 'id',
+    playbook_run_id = 'playbook_run_id',
+    receptor_job_id = 'receptor_job_id',
+    receptor_node_id = 'receptor_node_id',
+    status = 'status',
+    updated_at = 'updated_at'
+}
+
+export enum PlaybookRunSystem {
+    TABLE = 'playbook_run_systems',
+    playbook_run_executor_id = 'playbook_run_executor_id',
+    sequence = 'sequence',
+    status = 'status',
+    system_name = 'system_name',
+    console = 'console',
+    updated_at = 'updated_at'
+}
