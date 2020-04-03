9712cde4ea87987e60c225308ad28967faf8ab55
Alec Cohan <alcohan@redhat.com>
2020-04-03 14:49:29 -0400

    added cloudwatch logging to remediations-consumer and cleaner

--- src/util/log.ts
+++ src/util/log.ts
@@ -1,13 +1,37 @@
 import * as pino from 'pino';
 import config from '../config';
+import * as pinoms from 'pino-multi-stream';
+import * as pinoCW from 'pino-cloudwatch';
+
+function buildDestination () {
+    if (!config.logging.cloudwatch.enabled) {
+        return pino.destination(1); // stdout
+    }
+
+    const cwOptions = {
+        group: config.logging.cloudwatch.group,
+        prefix: config.logging.cloudwatch.prefix,
+        interval: config.logging.cloudwatch.interval,
+        aws_access_key_id: config.logging.cloudwatch.key,
+        aws_secret_access_key: config.logging.cloudwatch.secret,
+        aws_region: config.logging.cloudwatch.region
+    };
+
+    return pinoms.multistream([{
+        stream: pino.destination(1),
+        level: config.logging.level as pino.Level
+    }, {
+        stream: pinoCW(cwOptions),
+        level: config.logging.cloudwatch.level as pino.Level
+    }]);
+}
 
 const logger: pino.Logger = pino({
     name: 'remediations-consumer',
     level: config.logging.level,
-    prettyPrint: config.logging.pretty ? {
+    prettyPrint: config.logging.pretty && !config.logging.cloudwatch.enabled ? {
         errorProps: '*'
     } : false
-});
+}, buildDestination());
 
 export default logger.child({ type: 'application' });
-
