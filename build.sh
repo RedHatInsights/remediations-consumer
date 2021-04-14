3ae364f396866acee460de5587910920aaa65e27
Stephen Adams <tsadams@gmail.com>
2021-04-14 12:50:08 -0400

    [WIP] set up files for app-interface builds
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

---
+++ build.sh
@@ -0,0 +1,18 @@
+set -exv
+
+IMAGE="quay.io/cloudservices/remediations-consumer"
+IMAGE_TAG=$(git rev-parse --short=7 HEAD)
+SMOKE_TEST_TAG="latest"
+
+if [[ -z "$QUAY_USER" || -z "$QUAY_TOKEN" ]]; then
+    echo "QUAY_USER and QUAY_TOKEN must be set"
+    exit 1
+fi
+
+DOCKER_CONF="$PWD/.docker"
+mkdir -p "$DOCKER_CONF"
+docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
+docker --config="$DOCKER_CONF" build -f build/Dockerfile -t "${IMAGE}:${IMAGE_TAG}" . 
+docker --config="$DOCKER_CONF" push "${IMAGE}:${IMAGE_TAG}"
+docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:${SMOKE_TEST_TAG}"
+docker --config="$DOCKER_CONF" push "${IMAGE}:${SMOKE_TEST_TAG}"
