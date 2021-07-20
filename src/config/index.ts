85c0717e48603e4ace1c36092a1ed25ac907922c
Alec Cohan <alcohan@redhat.com>
2021-07-20 09:57:23 -0400

    Update linting errors

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,6 @@
 import * as convict from 'convict';
 import * as _ from 'lodash';
 import * as process from 'process';
-import * as fs from 'fs';
-import * as tmp from 'tmp';
 import formats from './formats';
 
 convict.addFormats(formats);
