ccf1072d9fc1c7b4a690d872509c1681c3d4f585
Rex White <rexwhite@redhat.com>
2022-09-02 18:14:13 -0400

    More PR unit-test testing...

--- unit_test.sh
+++ unit_test.sh
@@ -4,7 +4,7 @@ source $CICD_ROOT/deploy_ephemeral_db.sh
 # Map env vars set by `deploy_ephemeral_db.sh` if vars the app uses are different
 export DB_PASSWORD=$DATABASE_ADMIN_PASSWORD
 export DB_USERNAME=$DATABASE_ADMIN_USERNAME
-export DB_DATABASE="remediations_consumer_test"
+export DB_DATABASE=$DATABASE_NAME
 export DB_HOST=$DATABASE_HOST
 export DB_PORT=$DATABASE_PORT
 
@@ -16,7 +16,7 @@ echo DB_PORT:     $DB_PORT
 
 # run unit-tests
 npm ci
-npm run test-jenkins
+npm run test
 result=$?
 
 # TODO: add unittest-xml-reporting to rbac so that junit results can be parsed by jenkins
