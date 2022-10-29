10f686d6864e546bf2a6bb9884d2fde4fc17933b
Rex White <rexwhite@redhat.com>
2022-10-29 18:57:09 -0400

    Upgraded to node.js v16

--- unit_test.sh
+++ unit_test.sh
@@ -1,10 +1,8 @@
-# Docker version 20.10.14, build a224086
-# docker-compose version 1.29.1, build c34c88b2
-# podman version 1.6.4
-
-# run tests...
+# run our tests...
 docker-compose -f build/docker-compose-unit_test.yml up --exit-code-from remediations-consumer
+
 result=$?
+
 docker-compose -f build/docker-compose-unit_test.yml down
 
 # TODO: add unittest-xml-reporting to rbac so that junit results can be parsed by jenkins
