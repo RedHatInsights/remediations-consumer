259dc0eeb816a478c2f919a5f955a46b00bc6cf1
Rex White <rexwhite@redhat.com>
2022-10-29 18:01:53 -0400

    Switched to docker-compose for unit tests

--- pr_check.sh
+++ pr_check.sh
@@ -18,15 +18,6 @@ DB_DEPLOYMENT_NAME=remediations-db
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
 curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
-# DELETE ME - env tests
-echo "================== test stuff ===================="
-docker -v
-docker-compose -v
-podman -v
-# Docker version 20.10.14, build a224086
-# docker-compose version 1.29.1, build c34c88b2
-echo "============= END OF test stuff ===================="
-
 # Build Remediations image based on the latest commit
 source $CICD_ROOT/build.sh
 source $APP_ROOT/unit_test.sh
