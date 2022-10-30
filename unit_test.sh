3ced7e1d12ab656f9942e6da219a4c21fef2f2ee
Rex White <rexwhite@redhat.com>
2022-10-30 12:56:38 -0400

    Tidied up unit_test.sh

--- unit_test.sh
+++ unit_test.sh
@@ -1,10 +1,16 @@
 # run our tests...
 docker-compose -f build/docker-compose-unit_test.yml up --build --exit-code-from remediations-consumer
 
+# save result...
 result=$?
 
+# tidy up...
 docker-compose -f build/docker-compose-unit_test.yml down
 
-echo $WORKSPACE
-pwd
-ls
+# TODO: add unittest-xml-reporting to rbac so that junit results can be parsed by jenkins
+mkdir -p $WORKSPACE/artifacts
+cat << EOF > $WORKSPACE/artifacts/junit-dummy.xml
+<testsuite tests="1">
+    <testcase classname="dummy" name="dummytest"/>
+</testsuite>
+EOF
