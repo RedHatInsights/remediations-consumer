acef9bfda6773c85fe1c2404f094a623d04d90c6
Rex White <rexwhite@redhat.com>
2022-10-30 12:34:05 -0400

    Removed test coverage and changed jest-junit output dir

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
     coverageDirectory: 'artifacts/coverage',
     testEnvironment: 'node',
     preset: 'ts-jest',
-    // reporters: ['default', ['jest-junit', {JEST_JUNIT_OUTPUT_DIR: 'artifacts'}]],
-    reporters: ['default', ['jest-junit', {outputDirectory: 'artifacts', outputName: 'junit.xml'}]],
+    reporters: ['default', 'jest-junit'],
     testMatch: ['**/?(*.)+(unit|integration).ts']
 };
