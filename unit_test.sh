129b530fa94da2413bf06c7e801025185b628a77
Rex White <rexwhite@redhat.com>
2022-10-31 19:53:21 -0400

    Remove junit-dummy.xml file

--- unit_test.sh
+++ unit_test.sh
@@ -7,11 +7,3 @@ result=$?
 
 # tidy up...
 docker-compose -f build/docker-compose-unit_test.yml down
-
-# TODO: add unittest-xml-reporting to rbac so that junit results can be parsed by jenkins
-mkdir -p $WORKSPACE/artifacts
-cat << EOF > $WORKSPACE/artifacts/junit-dummy.xml
-<testsuite tests="1">
-    <testcase classname="dummy" name="dummytest"/>
-</testsuite>
-EOF
