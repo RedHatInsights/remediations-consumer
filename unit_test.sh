2893be67c03a5dc2c9ed99931ca05ce9a44eca67
Rex White <rexwhite@redhat.com>
2022-10-31 17:09:48 -0400

    Debugging env var export

--- unit_test.sh
+++ unit_test.sh
@@ -1,5 +1,5 @@
-DOCKER_UID=$(id -u)
-DOCKER_GID=$(id -g)
+export DOCKER_UID=$(id -u)
+export DOCKER_GID=$(id -g)
 
 # run our tests...
 docker-compose -f build/docker-compose-unit_test.yml up --build --exit-code-from remediations-consumer
