1c16b5d42187af6ea71fd9b70237b6666c566c1a
Rex White <rexwhite@redhat.com>
2022-10-31 16:23:07 -0400

    Fixed build to not bork the jenkins workspace :)

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
     coverageDirectory: 'artifacts/coverage',
     testEnvironment: 'node',
     preset: 'ts-jest',
-    reporters: ['default', 'jest-junit'],
+    reporters: ['default', ['jest-junit', {outputDirectory: 'artifacts', outputName: 'junit-remediations-consumer.xml'}]],
     testMatch: ['**/?(*.)+(unit|integration).ts']
 };
