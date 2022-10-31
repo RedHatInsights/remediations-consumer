db62ae0c2b4ac5b09515066f6d08e6bc5e2380e5
Rex White <rexwhite@redhat.com>
2022-10-31 19:35:46 -0400

    Re-enabling junit.xml test info output (fingers crossed...)

--- unit_test.sh
+++ unit_test.sh
@@ -1,5 +1,3 @@
-export DOCKER_UID=$(id -u)
-export DOCKER_GID=$(id -g)
 
 # run our tests...
 docker-compose -f build/docker-compose-unit_test.yml up --build --exit-code-from remediations-consumer
@@ -9,11 +7,3 @@ result=$?
 
 # tidy up...
 docker-compose -f build/docker-compose-unit_test.yml down
-
-# TODO: add unittest-xml-reporting to rbac so that junit results can be parsed by jenkins
-mkdir -p $WORKSPACE/artifacts
-cat << EOF > $WORKSPACE/artifacts/junit-dummy.xml
-<testsuite tests="1">
-    <testcase classname="dummy" name="dummytest"/>
-</testsuite>
-EOF
