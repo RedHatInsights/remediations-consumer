97a6e643a78908ccf611133357030a0f60d1b632
Rex White <rexwhite@redhat.com>
2022-11-01 18:58:06 -0400

    Fixed convict validator error, mostly in cleaner/run.ts

--- src/config/formats.ts
+++ src/config/formats.ts
@@ -1,11 +1,10 @@
 import * as fs from 'fs';
 
 export default {
-    file: {
-        validate () {},
-        coerce (path: string) {
-            // eslint-disable-next-line security/detect-non-literal-fs-filename
-            return fs.readFileSync(path, 'utf8');
-        }
+    name: 'file',
+    validate () {},
+    coerce (path: string) {
+        // eslint-disable-next-line security/detect-non-literal-fs-filename
+        return fs.readFileSync(path, 'utf8');
     }
 };
