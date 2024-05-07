0e6a23a91cebc1a30506735fd600275c2293b82b
Rex White <rexwhite@redhat.com>
2024-05-07 14:25:14 -0400

    RHINENG-9800: Update image creation process

--- knexfile.js
+++ knexfile.js
@@ -7,10 +7,10 @@ const options = {
     ...buildConfiguration(config),
     migrations: {
         tableName: 'knex_migrations',
-        directory: 'dist/test/migrations'
+        directory: './dist/test/migrations'
     },
     seeds: {
-        directory: 'dist/test/seeds'
+        directory: './dist/test/seeds'
     }
 };
 
