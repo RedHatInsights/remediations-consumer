a03d50072aa0ea3c944480ae8569318315707176
Rex White <rexwhite@redhat.com>
2024-06-28 11:31:05 -0400

    RHINENG-10424: Use podman-compose for compatability with rhel8 jenkins nodes

--- unit_test.sh
+++ unit_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # run our tests...
-docker-compose -f build/docker-compose-unit_test.yml up --build --exit-code-from remediations-consumer
+podman-compose -f build/docker-compose-unit_test.yml up --build --exit-code-from remediations-consumer
 
 # save result...
 result=$?
 
 # tidy up...
-docker-compose -f build/docker-compose-unit_test.yml down
+podman-compose -f build/docker-compose-unit_test.yml down
