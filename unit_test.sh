85fa44d6d92e3376dc011eaa04885d5021b063b6
Rex White <rexwhite@redhat.com>
2024-07-23 19:08:13 -0400

    Removing podman-compose dependency from builds

--- unit_test.sh
+++ unit_test.sh
@@ -1,18 +1,107 @@
 #!/bin/bash
 
-# run our tests...
-podman-compose -f build/docker-compose-unit_test.yml up --build --exit-code-from remediations-consumer
+# The following are defined outside of this script:
+# DOCKERFILE
+# APP_ROOT
+# IMAGE_TAG
 
-# save result...
-result=$?
+cd $APP_ROOT
 
-# tidy up...
-podman-compose -f build/docker-compose-unit_test.yml down
+API_IMAGE="local/remediations-consumer-test-${IMAGE_TAG}"
+API_CONTAINER_NAME="remediations-consumer-test-${IMAGE_TAG}"
 
-# TODO: add unittest-xml-reporting to rbac so that junit results can be parsed by jenkins
+DB_IMAGE="quay.io/cloudservices/postgresql-rds:14"
+DB_CONTAINER_NAME="remediations-consumer-db-${IMAGE_TAG}"
+
+NETWORK="remediations-consumer-test-${IMAGE_TAG}"
+
+# cleanup function to tidy up after the test run
+function tidy_up {
+  echo '----> Tidying up...'
+
+	podman rm -f $DB_CONTAINER_ID || true
+	podman rm -f $API_CONTAINER_ID || true
+	podman network rm -f $NETWORK || true
+
+  podman rmi -f $API_IMAGE || true
+  podman rmi -f $DB_IMAGE || true
+
+  podman container prune --force || true
+}
+
+trap "tidy_up" EXIT SIGINT SIGTERM
+
+#---------------------
+# create test network
+#---------------------
+echo '----> Create network...'
+
+podman network create --driver bridge $NETWORK
+
+if [ $? -ne 0 ]; then
+	echo '====> FAILED creating test network'
+	exit 1
+fi
+
+#--------------------
+# start db container
+#--------------------
+echo '----> pull DB image...'
+podman pull $DB_IMAGE
+
+echo '----> start db container...'
+DB_CONTAINER_ID=$(podman run -d \
+	--name "${DB_CONTAINER_NAME}" \
+	--network "${NETWORK}" \
+	-e POSTGRESQL_USER="postgres_user" \
+	-e POSTGRESQL_PASSWORD="remediations" \
+	-e POSTGRESQL_DATABASE="remediations_consumer_test" \
+	${DB_IMAGE} || echo "0")
+
+if [[ "$DB_CONTAINER_ID" == "0" ]]; then
+	echo "====> FAILED to start DB container"
+	exit 1
+fi
+
+#-----------------------------------
+# start remediations-test container
+#-----------------------------------
+echo '----> build api test image...'
+podman build -f $DOCKERFILE --target test -t $API_IMAGE .
+
+echo '----> start api test container...'
+API_CONTAINER_ID=$(podman run -d \
+  --name "${API_CONTAINER_NAME}" \
+  --network "${NETWORK}" \
+  -e NODE_ENV="test" \
+  -e DB_HOST="${DB_CONTAINER_NAME}" \
+  $API_IMAGE \
+  /bin/bash -c 'sleep infinity' || echo "0")
+
+if [[ "$API_CONTAINER_ID" == "0" ]]; then
+	echo "Failed to start api container"
+	exit 1
+fi
+
+#-------------------------------------
+# run remediations-tests in container
+#-------------------------------------
+echo '----> run api tests...'
+podman exec $API_CONTAINER_ID /bin/bash -c 'npm run test:ci'
+TEST_RESULT=$?
+
+if [ $TEST_RESULT -ne 0 ]; then
+	echo '====> unit tests FAILED'
+	exit 1
+fi
+
+#----------------
+# report results
+#----------------
+echo '----> record results...'
 mkdir -p $WORKSPACE/artifacts
-cat << EOF > $WORKSPACE/artifacts/junit-dummy.xml
-<testsuite tests="1">
-    <testcase classname="dummy" name="dummytest"/>
-</testsuite>
-EOF
+podman cp $API_CONTAINER_ID:/opt/app-root/artifacts/. $WORKSPACE/artifacts
+
+echo '====> unit tests PASSED'
+
+tidy_up
